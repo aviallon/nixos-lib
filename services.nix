15db62413a1be8f03f1ff846627be1eba674fecf
Antoine Viallon <antoine@lesviallon.fr>
2022-01-22 20:43:20 +0100

    [Services] Re-enable ananicy-cpp with better config generation Also add fwupd

--- services.nix
+++ services.nix
@@ -31,15 +31,20 @@ in {
 
     services.printing.enable = desktopCfg.enable;
 
-    services.ananicy.enable = false;
+    services.fwupd.enable = true;
+
+    services.ananicy.enable = true;
     services.ananicy.package = pkgs.ananicy-cpp;
     services.ananicy.settings = {
       loglevel = "info";
       cgroup_realtime_workaround = false;
     };
-    services.ananicy.extraRules = ''
-      {"name": "cp", "type": "BG_CPUIO"}
-    '';
+    services.ananicy.extraRules = concatStringsSep "\n" ( forEach [
+      {
+        name = "cp";
+        type = "BG_CPUIO";
+      }
+    ] (x: builtins.toJSON x));
 
     services.avahi.enable = true; # .lan/.local resolution
     services.avahi.nssmdns = true; # .lan/.local resolution
