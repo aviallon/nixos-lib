5af504998668f9edd1de8a66bd4179a02e950ff0
Antoine Viallon <antoine@lesviallon.fr>
2022-01-24 10:43:56 +0100

    [Refactoring] Use default.nix instead of _main.nix

---
+++ default.nix
@@ -0,0 +1,18 @@
+{config, ...}:
+{
+  imports = [
+    ./general.nix
+    ./boot.nix
+    ./desktop.nix
+    # ./home-manager.nix
+    ./network.nix
+    ./packages.nix
+    ./services.nix
+    ./filesystems.nix
+    ./hardening.nix
+    ./hardware.nix
+    ./laptop.nix
+    # ./overlays.nix
+    ./non-persistence.nix
+  ];
+}
