b358a3d72d3958da60978d6d8f1729083be15cd6
Antoine Viallon <antoine@lesviallon.fr>
2022-01-25 12:05:14 +0100

    [NonPersistence] add neededForBoot to /persist and /var/log, and verify that they exist

--- non-persistence.nix
+++ non-persistence.nix
@@ -8,10 +8,32 @@ in
     enable = mkEnableOption "non-persitent root"; 
   };
   config = mkIf cfg.enable {
+    assertions = [
+      { assertion = hasAttr "/persist" config.fileSystems;
+        message = "A /persist partition is needed for non-persistence";
+      }
+      { assertion = hasAttr "/var/log" config.fileSystems;
+        message = "A /var/log separate parition is needed to avoid missing early logs.";
+      }
+    ];
+
     environment.etc = {
       nixos.source = "/persist/etc/nixos";
       NIXOS.source = "/persist/etc/NIXOS";
       machine-id.source = "/persist/etc/machine-id";
+    };
+
+
+    fileSystems = {
+      "/var/log" = {
+        neededForBoot = true;
+        autoFormat = true;
+        label = "nixos-persistent-logs";
+      };
+      "/persist" = {
+        neededForBoot = true;
+        label = "nixos-persistent-data";
+      };
     };
   };
 }
