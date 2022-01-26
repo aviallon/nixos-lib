549bb4d1c0fcd127d671fc6ab4313959af5d84a6
Antoine Viallon <antoine@lesviallon.fr>
2022-01-27 00:38:07 +0100

    [Overlays] Add missing description

--- overlays.nix
+++ overlays.nix
@@ -7,6 +7,8 @@ in
   options.aviallon.overlays = {
     enable = mkOption {
       default = true;
+      example = false;
+      documentation = "Wether to enable system-wide overlays or not";
       type = types.bool;
     };
   };
