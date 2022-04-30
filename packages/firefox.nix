3a399de72835aeedf005cee9e436d4ad4e09c897
Antoine Viallon <antoine@lesviallon.fr>
2022-04-30 16:19:49 +0200

    [Packages/Firefox] Add Grammalecte extension

--- packages/firefox.nix
+++ packages/firefox.nix
@@ -57,12 +57,17 @@ in pkgs.wrapFirefox pkgs.firefox-esr-unwrapped {
       Extensions = {
         Install = [
           "uBlock0@raymondhill.net"
+          "French-GC@grammalecte.net"
         ];             
       };
       ExtensionSettings = {
         "uBlock0@raymondhill.net" = {
           installation_mode = "force_installed";
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+        };
+        "French-GC@grammalecte.net" = {
+          installation_mode = "force_installed";
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/grammalecte-fr/latest.xpi";
         };
       };
       ExtensionUpdate = true;
