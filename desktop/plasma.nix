4b6e4940d3c95677e46558ee09108547e3575beb
Antoine Viallon <antoine@lesviallon.fr>
2022-07-18 22:04:22 +0200

    [Desktop/Plasma] Fix chromium integration with KDE Plasma

--- desktop/plasma.nix
+++ desktop/plasma.nix
@@ -17,6 +17,11 @@ in {
       "x ${config.users.users.sddm.home}/.cache"
     ];
 
+    environment.etc = {
+      "chromium/native-messaging-hosts/org.kde.plasma.browser_integration.json".source = 
+        "${pkgs.plasma-browser-integration}/etc/chromium/native-messaging-hosts/org.kde.plasma.browser_integration.json";
+    };
+
     # Prevents blinking cursor
     services.xserver.displayManager.sddm = {
       enable = true;
