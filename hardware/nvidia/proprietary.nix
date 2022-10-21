e69036f6e1fd35e37c1843d4fcfc2cae546bbceb
Antoine Viallon <antoine@lesviallon.fr>
2022-10-21 23:50:43 +0200

    [Hardware/Nvidia] Use new nvidia vaapi

--- hardware/nvidia/proprietary.nix
+++ hardware/nvidia/proprietary.nix
@@ -41,11 +41,13 @@ in {
       ];
 
       hardware.opengl.extraPackages = with pkgs; [
+        nvidia-vaapi-driver
         libvdpau-va-gl
         vaapiVdpau
       ];
 
       hardware.opengl.extraPackages32 = with pkgs.pkgsi686Linux; [
+        nvidia-vaapi-driver
         libvdpau-va-gl
         vaapiVdpau
       ];
