47bf44eb3ac9dd3e950af5ede02112774d389db6
Antoine Viallon <antoine@lesviallon.fr>
2022-10-22 13:35:41 +0200

    [Boot] Better boot console resolution
    Hopefully

--- boot.nix
+++ boot.nix
@@ -176,6 +176,7 @@ in
       loader.systemd-boot = {
         enable = cfg.efi && (!cfg.useGrub);
         configurationLimit = cfg.configurationLimit; 
+        consoleMode = "auto";
       };
 
       loader.generic-extlinux-compatible = {
