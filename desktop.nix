34e1e82f4fd6f425a88bf91a7302bae2c9b7ed33
Antoine Viallon <antoine@lesviallon.fr>
2022-12-01 00:35:00 +0100

    [Desktop] Add wireplumber config for Bluetooth devices

--- desktop.nix
+++ desktop.nix
@@ -114,6 +114,16 @@ in {
         ];
       };
     };
+    environment.etc = {
+    	"wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
+    		bluez_monitor.properties = {
+    			["bluez5.enable-sbc-xq"] = true,
+    			["bluez5.enable-msbc"] = true,
+    			["bluez5.enable-hw-volume"] = true,
+    			["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+    		}
+    	'';
+    };
     security.rtkit.enable = true; # Real-time support for pipewire
 
     security.polkit.enable = true; # Better interactive privilege prompts
