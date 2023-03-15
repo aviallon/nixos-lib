cff1f34d05f0a09f4908b9a6fb214748576db22a
Antoine Viallon <antoine@lesviallon.fr>
2023-03-15 09:13:40 +0100

    [Hardware/Nvidia] Move nvidia.nix into nvidia/default.nix

--- hardware/nvidia/default.nix
+++ hardware/nvidia/default.nix
@@ -1,8 +1,23 @@
-{config, ...}:
-{
+{config, pkgs, lib, ...}:
+with lib;
+let
+  cfg = config.aviallon.hardware.nvidia;
+in {
+
   imports = [
     ./vgpu.nix
     ./proprietary.nix
     ./opensource.nix
   ];
+
+  options.aviallon.hardware.nvidia = {
+    enable = mkEnableOption "enable Nvidia hardware config";
+    useProprietary = mkEnableOption "nvidia proprietary drivers";
+    saveAllVram = mkEnableOption "back up all VRAM in /tmp before going to sleep. May reduce artifacts after resuming";
+  };
+
+  config = mkIf cfg.enable {
+    hardware.opengl.driSupport32Bit = true;
+  };
+
 }
