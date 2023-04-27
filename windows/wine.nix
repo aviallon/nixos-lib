250820cb82ac18b338198522981e4f85967c846d
Antoine Viallon <antoine@lesviallon.fr>
2023-04-27 18:15:48 +0200

    [Windows/Wine] make wine package configurable

--- windows/wine.nix
+++ windows/wine.nix
@@ -1,13 +1,21 @@
 { config, pkgs, lib, ... }:
 with lib;
-{
+let
+  cfg = config.aviallon.windows.wine;
+in {
   options.aviallon.windows.wine = {
     enable = mkEnableOption "windows executable support on Linux";
+    package = mkOption {
+      description = "Wine package to use";
+      type = types.package;
+      default = pkgs.wineWowPackages.waylandFull;
+      example = pkgs.winePackages.stable;
+    };
   };
-  config = {
+  config = mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
       bottles
-      wineWowPackages.waylandFull
+      cfg.package
     ];
   };
 }
