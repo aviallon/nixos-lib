43aab48b1ee951bb776dff331b7b10ea1e2acaa5
Antoine Viallon <antoine@lesviallon.fr>
2023-05-06 23:22:44 +0200

    [Hardware] Remove redundant configuration

--- hardware/default.nix
+++ hardware/default.nix
@@ -15,14 +15,6 @@ in
     ./mesa.nix
   ];
 
-  config = {
-    boot.kernel.sysctl = {
-
-      # Why: https://www.phoronix.com/news/Ryzen-Segv-Response
-      # Workaround: https://forums.gentoo.org/viewtopic-p-2605135.html#2605135
-      "kernel.randomize_va_space" = mkIf (generalCfg.cpuVendor == "amd" && generalCfg.cpuArch == "znver1" ) (warn "Disable Adress Space Layout Randomization on Ryzen 1 CPU" 0);
-    };
-
-  };
+  config = {};
 
 }
