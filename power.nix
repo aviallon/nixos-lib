92b6e28cfe10d5a759dc8a68228352f4abe6b656
Antoine Viallon <antoine@lesviallon.fr>
2024-01-02 14:55:16 +0100

    [Power] Fix RAPL permission udev rule Stupid typo...

--- power.nix
+++ power.nix
@@ -117,7 +117,7 @@ in {
       ACTION!="remove", KERNEL=="BAT*", SUBSYSTEM=="power_supply", ATTR{status}=="Discharging", RUN+="${pkgs.systemd}/bin/systemctl start battery-power.target"
       ACTION!="remove", KERNEL=="BAT*", SUBSYSTEM=="power_supply", ATTR{status}=="Charging", RUN+="${pkgs.systemd}/bin/systemctl stop battery-power.target"
 
-      ACTION!="remove", DEVPATH=="*intel-rapl:*", SUBSYSTEM=="powercap", RUN+="${pkgs.coreutils}/bin/chmod g+r '/sys%p/ernegy_uj'"
+      ACTION!="remove", DEVPATH=="*intel-rapl:*", SUBSYSTEM=="powercap", RUN+="${pkgs.coreutils}/bin/chmod g+r '/sys%p/energy_uj'"
     '';
 
     users.groups.power = {};
