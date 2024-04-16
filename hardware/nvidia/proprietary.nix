4500c8d8878390962040e0f92c9d148255dba809
Antoine Viallon <antoine@lesviallon.fr>
2024-04-16 19:18:46 +0200

    [Hardware/Nvidia/Proprietary]: only disable Wayland by default if no Intel GPU is present.
    Ideally, we should check wether any other GPU with open-source drivers
    is present.

--- hardware/nvidia/proprietary.nix
+++ hardware/nvidia/proprietary.nix
@@ -76,7 +76,7 @@ in {
 
     services.xserver.exportConfiguration = true;
 
-    services.xserver.displayManager.sddm.wayland.enable = false; # Frequent issues with Nvidia GPUs
+    services.xserver.displayManager.sddm.wayland.enable = mkIf (!config.aviallon.hardware.intel.enable) (mkDefault false); # Frequent issues with Nvidia GPUs
 
     # Fix hybrid sleep with Nvidia GPU
     systemd.services.nvidia-suspend = {
