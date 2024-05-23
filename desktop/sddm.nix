337de7ccd45fce5e9d010f0d0b647a58fdb011a3
Antoine Viallon <antoine@lesviallon.fr>
2024-05-23 10:47:56 +0200

    [Desktop/SDDM] drop sddm-unstable hacks and uneeded config for NixOS 24.05

--- desktop/sddm.nix
+++ desktop/sddm.nix
@@ -7,30 +7,17 @@ in {
   options.aviallon.desktop.sddm.unstable = mkEnableOption "bleeding-edge SDDM";
   options.aviallon.desktop.sddm.enable = mkEnableOption "custom SDDM configuration";
 
-  imports = [ ./sddm-unstable.nix ];
-
   config = mkIf cfg.sddm.enable {
 
-    # Delete SDDM QMLCache
-    systemd.tmpfiles.rules = mkAfter [
-      "e ${config.users.users.sddm.home}/.cache/sddm-greeter/qmlcache/ - - - 0"
-    ];
-  
     # Prevents blinking cursor
-    services.xserver.displayManager.sddm = {
+    services.displayManager.sddm = {
       enable = true;
       wayland.enable = mkDefault true;
+      wayland.compositor = "kwin";
       settings = {
-        /*General.GreeterEnvironment = mkIf sddmCfg.wayland.enable (concatStringsSep "," [
-          "QT_WAYLAND_SHELL_INTEGRATION=layer-shell"
-          "QT_QPA_PLATFORM=wayland"
-        ]);*/
         Theme = {
           CursorTheme = "breeze_cursors";
         };
-        /*Wayland = mkIf sddmCfg.wayland.enable {
-          CompositorCommand = mkOverride 60 "${pkgs.libsForQt5.kwin}/bin/kwin_wayland --drm --no-lockscreen --no-global-shortcuts --locale1";
-        };*/
       };
     };
   
