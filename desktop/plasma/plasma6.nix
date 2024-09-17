2028b48f4078f9e656b9347b5e2d81cd683a2b89
Antoine Viallon <antoine@lesviallon.fr>
2024-09-17 21:47:36 +0200

    [Desktop/Plasma] backport kwin explicit sync patches

--- desktop/plasma/plasma6.nix
+++ desktop/plasma/plasma6.nix
@@ -16,5 +16,40 @@ in {
 
     # We prefer Plasma Wayland
     services.displayManager.defaultSession = "plasma";
+
+    # Backport explicit sync patches
+    nixpkgs.overlays = [(final: prev: {
+      kdePackages = prev.kdePackages // {
+        kwin = prev.kdePackages.kwin.overrideAttrs (old: {
+          patches = old.patches ++ [
+            (prev.fetchpatch {
+              url = "https://invent.kde.org/plasma/kwin/-/commit/c99b3d52f254f3ec60fa27b3100aee79bb57a5f2.diff";
+              hash = "sha256-2C5droOWbJjymz00ZfCYTT60qpXmfyvwGURZROTfzjQ=";
+            })
+            (prev.fetchpatch {
+              url = "https://invent.kde.org/plasma/kwin/-/commit/4aac3914b8744b41d7aa8d9440f19dca4ba3480c.diff";
+              hash = "sha256-qAgsiRGikIAu5+hZ/np4bAywORRx16zl5pJcZL/n3Jg=";
+            })
+            (prev.fetchpatch {
+              url = "https://invent.kde.org/plasma/kwin/-/commit/c56576a62a167930bf1ce7e0130b1ded17fb4efb.diff";
+              hash = "sha256-f9+1NUgsC18QUQZVRefDVnnKnMxJthBtj/SHQtZotgc=";
+            })
+            (prev.fetchpatch {
+              url = "https://invent.kde.org/plasma/kwin/-/commit/272e856780c7386ff1aa3e063975ab1624954a7d.diff";
+              hash = "sha256-kgzwhJ1qV78J6DbszVBS2uQfpy/1BVrdZ7IuRz3R4ps=";
+            })
+            (prev.fetchpatch {
+              url = "https://invent.kde.org/plasma/kwin/-/commit/a5bcbf1c37e8e3a15c26b9b634204f2846b45dde.diff";
+              hash = "sha256-Urx3NlaAwUoGds2UWQCeVtJxdIWJsfnSnIsZcXGA8MQ=";
+            })
+            (prev.fetchpatch {
+              url = "https://invent.kde.org/plasma/kwin/-/commit/b162003695f34d64fff929056245c2046cf42e65.diff";
+              hash = "sha256-prHXvmI8ByytOHgl90/BQe4Uuo3RYy+ByLlmGUXjf28=";
+            })
+          ];
+        });
+      };
+    })];
+    
   };
 }
