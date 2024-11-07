c500db4bcde64a40d59f4fa2110ebdec3d249609
Antoine Viallon <antoine@lesviallon.fr>
2024-11-07 15:10:19 +0100

    [Desktop/Multimedia] Try to fix Wireplumber config

--- desktop/multimedia.nix
+++ desktop/multimedia.nix
@@ -106,17 +106,18 @@ in {
         ];
       };
     };
-    
-    environment.etc = {
-    	"wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
-    		bluez_monitor.properties = {
-    			["bluez5.enable-sbc-xq"] = true,
-    			["bluez5.enable-msbc"] = true,
-    			["bluez5.enable-hw-volume"] = true,
-    			["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-    		}
-    	'';
+
+    # https://pipewire.pages.freedesktop.org/wireplumber/daemon/configuration/bluetooth.html
+    services.pipewire.wireplumber.extraConfig = {
+      "monitor.bluez.properties" = {
+        "bluez5.enable-sbc-xq" = true; # Should be default now
+        "bluez5.enable-msbc" = true; # Default
+        "bluez5.enable-hw-volume" = true; # Default
+        "bluez5.headset-roles" = [ "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
+      };
     };
+
+    
     security.rtkit.enable = true; # Real-time support for pipewire
 
     aviallon.programs.allowUnfreeList = [
